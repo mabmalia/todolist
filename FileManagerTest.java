ced3670a226ac4416297fc5e986cc27f48bc6635
mabmalia <54804983+mabmalia@users.noreply.github.com>
2019-10-10 17:58:24 +0200

    Add test file

---
+++ FileManagerTest.java
@@ -0,0 +1,50 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class FileManagerTest {
+
+    @Test
+    public void FolderExistsTrue(){
+
+        FileManager fileManager = new FileManager();
+
+        Path path = Paths.get(fileManager.toString().substring(0,fileManager.toString().lastIndexOf("/")));
+
+        assertTrue(Files.exists(path));
+    }
+
+    @Test
+    public void WriteFileTrue(){
+
+        //create a date and a list with some tasks
+        LocalDate date = Utility.convertDate("2020-10-20");
+
+        ArrayList<Task> listSample = new ArrayList<>();
+        listSample.add(new Task("bir,lz","projectX", date));
+        listSample.add(new Task("sam\"p\"le","shopping", date));
+
+        //write tasks to file
+        FileManager fileManager = new FileManager();
+        fileManager.writeToCSV(listSample);
+
+        //Check if file is empty
+        int fileSize;
+        try{
+            fileSize = Files.readAllLines(Paths.get(fileManager.toString())).size();
+        }
+        catch(IOException e){
+            e.printStackTrace();
+            fileSize = -1;
+        }
+
+        assertTrue(Files.exists(Paths.get(fileManager.toString())));
+        assertTrue(fileSize > 0);
+    }
+}
